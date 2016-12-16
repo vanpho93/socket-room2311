9844daaa205eb78da531a7dd6b6ecc60d4c8fbe9
Admin <vanpho02@gmail.com>
2016-12-16 11:31:32 +0700

    xong

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
-server.listen(3000, () => console.log('Server started'));
+server.listen(process.env.PORT || 3000, () => console.log('Server started'));
 
 function Room(roomName){
   this.roomName = roomName;
